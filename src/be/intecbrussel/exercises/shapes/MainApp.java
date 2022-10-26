4ada28ed73dfc272df557829ab7af92338be1eb0
Mano-lemm <lemmensmano@gmail.com>
2022-10-26 09:26:21 +0200

    use square in exercise

--- src/be/intecbrussel/exercises/shapes/MainApp.java
+++ src/be/intecbrussel/exercises/shapes/MainApp.java
@@ -4,6 +4,8 @@ public class MainApp {
     public static void main(String[] args) {
         Circle c1 = new Circle(2.0);
         Circle c2 = new Circle(2.0);
+        Square s1 = new Square(10);
         System.out.println(c1.equals(c2));
+        System.out.println(c1.equals(s1));
     } 
 }
